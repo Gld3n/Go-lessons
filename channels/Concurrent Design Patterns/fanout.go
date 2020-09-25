25824642a52bbf09adf765609f5ec4a0971939b0
Gld3n <robertvale301104@hotmail.com>
2020-09-25 16:27:40 -0400

    Added coments to fan out

--- channels/Concurrent Design Patterns/fanout.go
+++ channels/Concurrent Design Patterns/fanout.go
@@ -11,7 +11,7 @@ func main() {
 	c1 := make(chan int)
 	c2 := make(chan int)
 
-	go agregar(c1)
+	go addint(c1)
 
 	go fanOutIn(c1, c2)
 
@@ -22,27 +22,36 @@ func main() {
 	fmt.Println("about to exit")
 }
 
-func agregar(c chan int) {
+func addint(c chan int) {
+	// put the index number in c 100 times
 	for i := 0; i < 100; i++ {
 		c <- i
 	}
+	// close the channel to avoid blocking the execution when
+	// range is applied to c
 	close(c)
 }
 
+// FAN OUT
 func fanOutIn(c1, c2 chan int) {
 	var wg sync.WaitGroup
+	// cycle through c1 and launch a goroutine which puts in c2 what
+	// expensiveJob returns
 	for v := range c1 {
-		wg.Add(1)
+		wg.Add(1) // for each range and a goroutine
 		go func(v2 int) {
-			c2 <- trabajoConsumeTiempo(v2)
-			wg.Done()
+			c2 <- expensiveJob(v2)
+			wg.Done() // finish the goroutine
 		}(v)
 	}
 	wg.Wait()
+	// close c2 channel to prevent blocking execution when
+	// applying range to it
 	close(c2)
 }
 
-func trabajoConsumeTiempo(n int) int {
+func expensiveJob(n int) int {
+	// sets a random waiting time and returns a random integer
 	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
 	return n + rand.Intn(1000)
 }
